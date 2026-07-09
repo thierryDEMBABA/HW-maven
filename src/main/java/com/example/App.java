99b8f3b0e177b8bd8cc608190de73d5b946a858c
Thierry <thiernkenfack@gmail.com>
2026-07-09 12:49:28 +0200

    test du build automatique

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, World! Ce build a réussi grâce à Jenkins et Maven.");
+        System.out.println("Hello, World! Ce build a réussi grâce à Jenkins et Maven. Test du build automatisé.");
     }
 }
